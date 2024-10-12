1c023414897913a801831e540453bfcbeffdfda6
kamkalian <kurm_de+github@pm.me>
2024-10-12 07:32:41 +0000

    Remove obsolete imports

--- api/dependencies/db.py
+++ api/dependencies/db.py
@@ -4,22 +4,15 @@ from __future__ import annotations
 
 import contextlib
 import logging
-import typing as t
-import fastapi
 import db_connection
 
-import config as api_config
 import api.typing.db as db_types
-from config import get_dev_settings
 
 logger = logging.getLogger("app")
 
 
 @contextlib.asynccontextmanager
-async def _handle_mysql_connection_context(
-    settings: api_config.Settings = fastapi.Depends(get_dev_settings)
-):
-    
+async def _handle_mysql_connection_context():
     logger.debug("Create new MySQL DB connection.")
     async with db_connection.create_mysql_connection() as conn, conn.cursor() as cursor:
         try:
