b1bb2a0ac9f9e5fea557b21acce229e2620d2cbf
kamkalian <kurm_de+github@pm.me>
2024-12-04 12:53:24 +0000

    Implement QR-Code label

--- api/routers/task_qrcode.py
+++ api/routers/task_qrcode.py
@@ -7,10 +7,16 @@ import io
 from starlette.responses import StreamingResponse
 import qrcode
 import qrcode.image.svg
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 
-
+from api.auth import get_current_active_user
+from api.db_queries.qrcode import query_complete_print_job, query_get_print_jobs, query_insert_print_job
 from api.dependencies.db import get_db_connection
+from api.models.qrcode import PayloadPrintJob
+from api.models.user import UserFull
 
 if t.TYPE_CHECKING:
     import api.typing.db as db_types
@@ -29,6 +35,71 @@ async def qrcode_create(
     img.save(buf)
     buf.seek(0)
     return StreamingResponse(buf, media_type="image/jpeg")
+
+
+@router.get("/create_label")
+async def label_create(
+    db_conn: db_types.DBConnection = fastapi.Depends(get_db_connection),
+    *,
+    task_id: int,
+) -> StreamingResponse:
+    
+    label_image = Image.new(mode="RGB", size=(696, 200), color="white")
+    
+    awo_logo = Image.open("public/images/awo-logo.png")
+    awo_logo = awo_logo.resize((140, 120))
+
+    qrcode_image = qrcode.make("https://reparaturcafe-dev.it-awo.de/task/"+str(task_id))
+    qrcode_image = qrcode_image.resize((180, 180))
+
+    label_image.paste(qrcode_image, (10, 10))
+    label_image.paste(awo_logo, (520, 40), awo_logo)
+
+    draw = ImageDraw.Draw(label_image)
+    font = ImageFont.load_default(40)
+    font
+    draw.text((210, 30),"ID: " + str(task_id), font=font, fill=(0,0,0))
+    draw.text((210, 80),"ReparaturCafe", font=font, fill=(0,0,0))
+    draw.text((210, 130),"AWO Oberlar e.V.", font=font, fill=(0,0,0))
+
+
+    buf = io.BytesIO()
+    label_image.save(buf, "JPEG")
+    buf.seek(0)
+
+    return StreamingResponse(buf, media_type="image/jpeg")
+
+
+@router.post("/print")
+async def qrcode_print(
+    db_conn: db_types.DBConnection = fastapi.Depends(get_db_connection),
+    *,
+    task_id: int,
+):
+    await query_insert_print_job(db_conn=db_conn, task_id=task_id)
+    await db_conn.connection.commit()
+
+
+@router.patch("/complete")
+async def complete_qrcode_print(
+    db_conn: db_types.DBConnection = fastapi.Depends(get_db_connection),
+    *,
+    qrcode_id: int,
+):
+    await query_complete_print_job(db_conn=db_conn, qrcode_id=qrcode_id)
+    await db_conn.connection.commit()
+
+
+@router.get("/all", response_model=list[PayloadPrintJob], dependencies=[fastapi.Depends(get_current_active_user)])
+async def get_all_print_jobs(
+    db_conn: db_types.DBConnection = fastapi.Depends(get_db_connection),
+    *,
+    current_user: t.Annotated[UserFull, fastapi.Depends(get_current_active_user)],
+):
+    print_jobs: list[PayloadPrintJob] = await query_get_print_jobs(db_conn=db_conn)
+    for job in print_jobs:
+        await complete_qrcode_print(db_conn=db_conn, qrcode_id=job.id)
+    return print_jobs
 
 
 
